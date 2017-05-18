933b1a55c4f5deafe259ab69853ca12a40242d57
b-boyila <b-boyila@mail.ru>
2017-05-18 14:01:39 +0500

    Расчёт интервала, вывод истории сделок с btc-e.nz

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,20 @@
 /**
  * Created by ktagintsev on 15.05.17.
  */
+var step;
 
 function closeAlert(obj) {
     $(obj).alert('close');
+}
+
+function changeStep() {
+	step = $('#step-ct').val();
+	var interval = step * 30;
+	$("#interval-ct").text("Интервал = " + interval);
+}
+
+function analyze() {
+	$.post("/processors/btc/macd.php", {step: step}, function(data) {
+  		$('.result').html(data);
+	});
 }
